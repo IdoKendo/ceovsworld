95daab57e8c0c8829a6c8c1086a6a6fbdde9dc58
IdoKendo <ryuusuke@gmail.com>
2018-12-05 10:22:00 +0200

    [+] Add initial scenes/work

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] private float m_timeToWait = 3.5f;
+
+    private int currentSceneIdx;
+
+    private void Start()
+    {
+        currentSceneIdx = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentSceneIdx == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+    private IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(m_timeToWait);
+
+        LoadNextScene();
+
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIdx + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/TypeWriterEffect.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypeWriterEffect : MonoBehaviour
+{
+    [SerializeField] private float m_typingDelay = 0.2f;
+    [SerializeField] private float m_linesDelay = 0.4f;
+    [SerializeField] private List<string> m_textLines = new List<string>();
+
+    private string m_currentText = string.Empty;
+    private Text m_textBox;
+
+    private int currentLine;
+    private int textIndex;
+
+
+    private void Start()
+    {
+        m_textBox = GetComponent<Text>();
+
+        StartCoroutine(ShowText());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("return"))
+        {
+            if (textIndex < m_textLines[currentLine].Length)
+            {
+                textIndex = m_textLines[currentLine].Length;
+            }
+            else if (currentLine < m_textLines.Count)
+            {
+                textIndex = 0;
+                currentLine++;
+            }
+            else
+            {
+                HideDialogueBox();
+            }
+        }
+    }
+
+    private IEnumerator ShowText()
+    {
+        for (currentLine = 0; currentLine < m_textLines.Count; currentLine++)
+        {
+            for (textIndex = 0; textIndex <= m_textLines[currentLine].Length; textIndex++)
+            {
+                m_currentText = m_textLines[currentLine].Substring(0, textIndex);
+                m_textBox.text = m_currentText.ToUpper();
+
+                yield return new WaitForSeconds(m_typingDelay);
+            }
+
+            yield return new WaitForSeconds(m_typingDelay);
+        }
+    }
+
+    private void HideDialogueBox()
+    {
+        Debug.Log("Hiding DialogueBox");
+    }
+}
