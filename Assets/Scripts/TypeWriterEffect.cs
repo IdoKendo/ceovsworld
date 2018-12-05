9be87c4cc8d227822f83288cd0afa67584fd23ce
IdoKendo <ryuusuke@gmail.com>
2018-12-05 11:09:45 +0200

    [+] Implement DialogueBox prefab & code

--- Assets/Scripts/TypeWriterEffect.cs
+++ Assets/Scripts/TypeWriterEffect.cs
@@ -8,17 +8,22 @@ public class TypeWriterEffect : MonoBehaviour
     [SerializeField] private float m_typingDelay = 0.2f;
     [SerializeField] private float m_linesDelay = 0.4f;
     [SerializeField] private List<string> m_textLines = new List<string>();
+    [SerializeField] private List<Sprite> m_speakers = new List<Sprite>();
+    [SerializeField] private int m_changeSpeakerLine = 4;
 
     private string m_currentText = string.Empty;
     private Text m_textBox;
-
-    private int currentLine;
-    private int textIndex;
-
+    private Image m_currentSpeaker;
+    private Image m_dialogueBox;
+    private int m_currentLine;
+    private int m_textIndex;
+    private int m_speakerIndex;
 
     private void Start()
     {
         m_textBox = GetComponent<Text>();
+        m_currentSpeaker = GetComponentInChildren<Image>();
+        m_dialogueBox = GetComponentInParent<Image>();
 
         StartCoroutine(ShowText());
     }
@@ -27,40 +32,42 @@ public class TypeWriterEffect : MonoBehaviour
     {
         if (Input.GetKeyDown("return"))
         {
-            if (textIndex < m_textLines[currentLine].Length)
+            if (m_textIndex <= m_textLines[m_currentLine].Length - 3)
             {
-                textIndex = m_textLines[currentLine].Length;
-            }
-            else if (currentLine < m_textLines.Count)
-            {
-                textIndex = 0;
-                currentLine++;
-            }
-            else
-            {
-                HideDialogueBox();
+                m_textIndex = m_textLines[m_currentLine].Length - 3;
             }
         }
     }
 
     private IEnumerator ShowText()
     {
-        for (currentLine = 0; currentLine < m_textLines.Count; currentLine++)
+        for (m_currentLine = 0; m_currentLine < m_textLines.Count; m_currentLine++)
         {
-            for (textIndex = 0; textIndex <= m_textLines[currentLine].Length; textIndex++)
+            if (m_currentLine == m_changeSpeakerLine)
             {
-                m_currentText = m_textLines[currentLine].Substring(0, textIndex);
+                m_speakerIndex++;
+            }
+
+            m_currentSpeaker.sprite = m_speakers[m_speakerIndex];
+
+            for (m_textIndex = 0; m_textIndex <= m_textLines[m_currentLine].Length; m_textIndex++)
+            {
+                m_currentText = m_textLines[m_currentLine].Substring(0, m_textIndex);
                 m_textBox.text = m_currentText.ToUpper();
 
                 yield return new WaitForSeconds(m_typingDelay);
             }
 
-            yield return new WaitForSeconds(m_typingDelay);
+            yield return new WaitForSeconds(m_linesDelay);
         }
+
+        HideDialogueBox();
     }
 
     private void HideDialogueBox()
     {
-        Debug.Log("Hiding DialogueBox");
+        Destroy(m_dialogueBox);
+        Destroy(m_currentSpeaker);
+        Destroy(gameObject);
     }
 }
